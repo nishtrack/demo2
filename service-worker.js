12084d8456c1ce26d4306548e42ff8eed4ea893c
nishant dixti <nishantdixit24@gmail.com>
2017-12-30 14:40:50 +0530

    sw

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+var version = '2.1';
+var NOTIFYVISITORS_BRAIND_ID = '1363';
+importScripts('https://devpush.notifyvisitors.com/js/brand_hosted/push-worker.js');
+//importScripts('https://s3.amazonaws.com/notifypush/cache_worker/config-5656.js');
+//importScripts('https://devpush.notifyvisitors.com/js/brand_hosted/cache-worker.js');
