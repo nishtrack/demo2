9285b8973e4fbcbf2842b52a361494c736c2b2dd
nishant dixti <nishantdixit24@gmail.com>
2017-12-30 14:47:03 +0530

    index.js

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/login', function(request, response) {
 response.sendfile('./index.html');
 })
 
+app.get('/:name', function(request, response) {
+
+   var options = {
+       root: __dirname + '/',
+       headers: {
+          'x-timestamp': Date.now(),
+           'x-sent': true
+        }
+    }; 
+
+   var fileName = request.params.name;
+    response.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
